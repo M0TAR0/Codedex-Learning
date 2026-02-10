c99a25a873578c33bf792732e9be3483aec60612
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-10 10:58:48 -0600

    Finished Chapter 2 Javascript

---
+++ Courses Exercises/JavaScript/Chapter2_Variables/Ex. 10 Planet Weight.js
@@ -0,0 +1,8 @@
+let earthWeight = 54; //Kg
+let destination_weight;
+let marsConversionRate = 0.38;
+
+destination_weight = earthWeight*marsConversionRate
+
+console.log("Your weight on Earth is " + earthWeight + " pounds.");
+console.log("Your weight on Mars is " + destination_weight + " pounds.");
