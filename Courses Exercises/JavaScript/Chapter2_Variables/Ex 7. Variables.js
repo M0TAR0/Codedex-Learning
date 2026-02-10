e5ee9bd1f267fd32d1ad4fc6882d2b1742bf92ac
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-10 10:47:06 -0600

    Ex 7 JS

---
+++ Courses Exercises/JavaScript/Chapter2_Variables/Ex 7. Variables.js
@@ -0,0 +1,9 @@
+let companyName = "This is a String";
+const foundingYear = 1942;
+let isActive = false;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
