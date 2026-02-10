b8b75d1c642b6cc8069170b620e8505eee2f283a
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-10 10:42:03 -0600

    Exercise 6 JS

---
+++ Courses Exercises/JavaScript/Chapter2_Variables/Ex. 6 Let & Const.js
@@ -0,0 +1,14 @@
+const firstName = "Romel";
+const favoriteColor = "Black";
+
+let location = "CDMX";
+let mood = "Happy";
+
+console.log(firstName);
+console.log(favoriteColor);
+console.log(location);
+console.log(mood);
+
+mood = "Angry";
+
+console.log(mood);
