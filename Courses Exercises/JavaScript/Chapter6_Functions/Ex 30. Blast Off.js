528b298120d35fcd583ed2056aebd256fda40061
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-20 13:57:13 -0600

    Added JS 6 Exercise

---
+++ Courses Exercises/JavaScript/Chapter6_Functions/Ex 30. Blast Off.js
@@ -0,0 +1,10 @@
+// Write code below 💖
+
+function countdown() {
+  for (let i = 10; i > 0; i--){
+    console.log(i);
+  }
+  return "Blast Off! 🚀";
+}
+
+console.log(countdown());
---
+++ Courses Exercises/JavaScript/Chapter6_Functions/Ex 31. Emc.js
@@ -0,0 +1,9 @@
+//Write your ocde here.
+
+function relativityTheory(mass){
+    let energy;
+    energy = mass*(300000**2);
+    return energy;
+}
+
+console.log(relativityTheory(20));
