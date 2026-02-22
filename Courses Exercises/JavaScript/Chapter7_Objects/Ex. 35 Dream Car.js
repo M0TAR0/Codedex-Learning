4279d1a115a2f796adbcba9c6fcc0a077d710ac9
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-22 17:56:24 -0600

    Exercise 35 JS7

---
+++ Courses Exercises/JavaScript/Chapter7_Objects/Ex. 35 Dream Car.js
@@ -0,0 +1,12 @@
+const car = {
+    model: "Ferrari",
+    year: 1950,
+    color: "red",
+    used: false
+};
+
+if (car.used = true){
+    console.log("I'm looking for a", car.color, car.year, car.model, "that is used.");
+} else {
+    console.log("I'm looking for a", car.color, car.year, car.model, "that is new.");
+}
