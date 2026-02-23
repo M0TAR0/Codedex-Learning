9370e2a85ed25623089f9e7ccb67d22e7e2c50cc
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-22 18:14:28 -0600

    Exercise 36 JS7

---
+++ Courses Exercises/JavaScript/Chapter7_Objects/Ex. 36 Pokemon.js
@@ -0,0 +1,14 @@
+pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25,
+}
+
+pokemon.isCaught = false;
+
+console.log(pokemon.isCaught);
+
+pokemon.isCaught = true;
+pokemon.name = "Pikachu";
+
+console.log(pokemon);
