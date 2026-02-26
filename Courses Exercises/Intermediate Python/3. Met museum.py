5c98401a7df0ce9a8a4c34ec56e21d1a4770095c
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-26 09:48:19 -0600

    Exercise 3 - Int Python

---
+++ Courses Exercises/Intermediate Python/3. Met museum.py
@@ -0,0 +1,14 @@
+Artwork_Details = {
+    'Title': "Still Life-Violin and Music",
+    'Artist': "William Michael Harnett",
+    'Period': "1848-1892",
+    'Date': 1888,
+    'Culture': "American",
+    'Medium': "Oil on canvas",
+    'Object_Number': 63.85,
+}
+
+print(Artwork_Details)
+print(Artwork_Details.keys())
+print(Artwork_Details.values())
+print(Artwork_Details.Title)
