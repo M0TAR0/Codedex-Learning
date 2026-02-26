c07ef2d35d59e8565329a9046d570a67f30b2056
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-26 09:48:42 -0600

    Exercise 4 Int Python

---
+++ Courses Exercises/Intermediate Python/4. Fruits.py
@@ -0,0 +1,8 @@
+romel_fruits = {"Grapes", "Apple", "Banana", "Watermelon"}
+romina_fruits = {"Mango", "Lemon", "Tomatoes", "Apple",}
+
+print(romel_fruits)
+print(romina_fruits)
+
+print("Intersection: ", romel_fruits.intersection(romina_fruits))
+print("Difference: ", romel_fruits.difference(romina_fruits))
